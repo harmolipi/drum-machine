663fa87620460f4440c67392b786ee469cb0de2d
harmolipi <nikob381@gmail.com>
2022-01-12 13:56:44 -0500

    Styled drum display

--- src/components/DrumDisplay.js
+++ src/components/DrumDisplay.js
@@ -1,5 +1,12 @@
 const DrumDisplay = ({ drum }) => {
-  return <div id="display">{drum}</div>;
+  return (
+    <div
+      id="display"
+      class="border-solid border-2 border-slate-400 p-2 mx-auto w-full text-center"
+    >
+      {drum}
+    </div>
+  );
 };
 
 export default DrumDisplay;
